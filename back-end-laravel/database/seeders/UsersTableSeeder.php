cbcc5d8fa8c91a48012c39dbcb63962f4088aaa3
Matteo0421 <matteo.carola99@gmail.com>
2024-06-14 12:57:05 +0200

    [add]user

--- back-end-laravel/database/seeders/UsersTableSeeder.php
+++ back-end-laravel/database/seeders/UsersTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
+      $new_user = new User();
+      $new_user->name = 'Alessandro';
+      $new_user->email = 'alessandro@gmail.com';
+      $new_user->password = Hash::make('12341234');
+
+      $new_user->save();
 
     }
 }
