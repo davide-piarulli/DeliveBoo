75313d3b762a9f33b9d993ef6e892f9f68d00102
s4r44d43 <sararez@live.it>
2024-06-18 16:59:53 +0200

    [grafica] dashboard

--- back-end-laravel/resources/views/admin/partials/aside.blade.php
+++ back-end-laravel/resources/views/admin/partials/aside.blade.php
@@ -1,17 +1,17 @@
 <aside class="bg-dark">
   <nav>
     <ul>
-
-      <li>
-        <a href="{{ route('admin.products.index') }}">
-          <i class="fa-solid fa-house   p-2 me-1  mt-2"> Elenco Prodotti </i>
-        </a>
-
+      <li class="nav-item">
+        <a href="{{ route('admin.products.index') }}" class="nav-link text-white">
+          <i class="fa-solid fa-house"></i>
+          <span class="d-none d-lg-inline-block">Elenco Prodotto</span>
+          <a>
       </li>
-      <li>
-        <a href="{{ route('admin.products.create') }}">
-          <i class="fa-solid fa-chart-simple p-2 me-1 mt-2 "> Nuovo Prodotto</i>
-        </a>
+      <li class="nav-item">
+        <a href="{{ route('admin.products.create') }}" class="nav-link text-white">
+          <i class="fa-solid fa-chart-simple"></i>
+          <span class="d-none d-lg-inline-block">Nuovo Prodotto</span>
+          <a>
       </li>
 
       {{-- <li>
--- back-end-laravel/resources/views/admin/partials/header.blade.php
+++ back-end-laravel/resources/views/admin/partials/header.blade.php
@@ -1,12 +1,12 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark h-100">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                    <li class="nav-item home_link ">
                         <a class="nav-link" href="{{ route('admin.home')}}">Home</a>
                     </li>
                     <li class="nav-item">
@@ -15,7 +15,7 @@
                 </ul>
 
                 <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
+                    <li class="nav-item mx-4">
                         <a class="nav-link" aria-current="page">{{ Auth::user()->name }}</a>
                     </li>
                     <li class="nav-item">
