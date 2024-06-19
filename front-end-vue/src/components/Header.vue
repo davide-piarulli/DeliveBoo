6f825d001d5ee98368f592cdcf2dcd15f073db58
s4r44d43 <sararez@live.it>
2024-06-19 17:48:56 +0200

    [header]

--- front-end-vue/src/components/Header.vue
+++ front-end-vue/src/components/Header.vue
@@ -6,17 +6,23 @@ export default {
 
 <template>
   <header>
-    <nav class="navbar bg-body-tertiary h-100">
-      <div class="container-fluid">
+    <nav class="navbar h-100">
+      <div class="container">
         <router-link :to="{ name: 'home' }" class="navbar-brand">
-          <img src="/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
-          DELIVEBOO
+          <img src="/logo.png" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
         </router-link>
-        <ul class="d-flex justify-content-around">
-          <li class="mx-4 link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-            <router-link :to="{ name: 'home' }">Home</router-link>
+        <ul>
+          <li class="mx-4 link-info link-offset-2 d-none d-md-inline-block fs-6">
+            <router-link class="white_text" :to="{ name: 'home' }">Home</router-link>
+          </li>
+          <li class="mx-4 link-info link-offset-2 d-none d-md-inline-block fs-6">
+            <a class="white_text" href="#">Ristoranti</a>
+          </li>
+          <li class="me-4 fs-2 d-md-none">
+            <i class="fa-solid fa-bars"></i>
           </li>
         </ul>
+
       </div>
     </nav>
   </header>
@@ -24,7 +30,29 @@ export default {
 
 
 <style lang="scss" scoped>
+
+@use '../assets/scss/main.scss' as*;
+
 header {
+  position: relative;
   height: 100px;
+  nav{
+    background-color: $color-1;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    li{
+      color: $color-10 !important;
+      i{
+        z-index: 999;
+      }
+    }
+  }
 }
+
+.white_text{
+  color: $color-10;
+}
+
 </style>
