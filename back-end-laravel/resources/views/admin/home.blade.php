afd82b94d5836b4dfd8bf188a0fb57192fce2a69
davide-piarulli <davide.piarulli.1@gmail.com>
2024-06-21 09:45:03 +0100

    fix home backend

--- back-end-laravel/resources/views/admin/home.blade.php
+++ back-end-laravel/resources/views/admin/home.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.admin')
 
 @section('content')
-
-<h2 class="text-center">Nel nostro database abbiamo {{$n_products}} prodotti</h2>
-
+  <h2 class="text-center">Nell'archivio ci sono: {{ $n_products }} prodotti</h2>
 @endsection
