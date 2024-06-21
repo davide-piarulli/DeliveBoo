88032efa65d5eebdeee2f4e77d0baded8a4f44b3
davide-piarulli <davide.piarulli.1@gmail.com>
2024-06-21 10:46:12 +0100

    [add] campi obbligatori Nuovo Prodotto

--- back-end-laravel/resources/views/admin/products/create.blade.php
+++ back-end-laravel/resources/views/admin/products/create.blade.php
@@ -15,8 +15,10 @@
       <div class="col-md-6">
         <form id="productForm" action="{{ route('admin.products.store') }}" method="POST" enctype="multipart/form-data">
           @csrf
+
+          {{-- Nome Prodotto --}}
           <div class="mb-3">
-            <label for="name" class="form-label">Nome *</label>
+            <label for="name" class="form-label">Nome<span class="fw-bold text-danger"> *</span></label>
             <input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name"
               value="{{ old('name') }}">
             <small id="nameError" class="text-danger"></small>
@@ -24,6 +26,8 @@
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
+
+          {{-- Descrizione Prodotto --}}
           <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
             <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description"
@@ -32,8 +36,10 @@
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
+
+          {{-- Prezzo Prodotto --}}
           <div class="mb-3">
-            <label for="price" class="form-label">Prezzo *</label>
+            <label for="price" class="form-label">Prezzo<span class="fw-bold text-danger"> *</span></label>
             <input name="price" type="number" step="0.01" class="form-control @error('price') is-invalid @enderror"
               id="price" value="{{ old('price') }}">
             <small id="priceError" class="text-danger"></small>
@@ -41,23 +47,34 @@
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
+
+          {{-- Tipo di Prodotto --}}
           <div class="mb-3">
-            <label class="form-label">Tipi di Prodotto: *</label>
-            <div class="btn-group btn-group-sm" role="group">
-              @foreach ($productTypes as $productType)
-                <input name="product_type_id" id="product_type_{{ $productType->id }}" class="btn-check"
-                  autocomplete="off" type="radio" value="{{ $productType->id }}"
-                  @if (old('product_type_id') == $productType->id) checked @endif>
-                <label class="btn btn-outline-primary"
-                  for="product_type_{{ $productType->id }}">{{ $productType->name }}</label>
-              @endforeach
+            <label class="form-label">Tipi di Prodotto:<span class="fw-bold text-danger"> *</span></label>
+            <div class="container-fluid">
+              <div class="row row-cols-2 row-cols-lg-4">
+                @foreach ($productTypes as $productType)
+                  <div class="col mb-3">
+                    <div class="btn-group btn-group-sm w-100" role="group">
+                      <input name="product_type_id" id="product_type_{{ $productType->id }}" class="btn-check"
+                        autocomplete="off" type="radio" value="{{ $productType->id }}"
+                        @if (old('product_type_id') == $productType->id) checked @endif>
+                      <label class="btn w-100"
+                        for="product_type_{{ $productType->id }}">{{ $productType->name }}</label>
+                    </div>
+                  </div>
+                @endforeach
+              </div>
             </div>
+
             <small id="productTypeValidation" class="text-danger"></small>
             @error('product_type_id')
               <br>
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
+
+          {{-- Immagine del Prodotto --}}
           <div class="mb-3">
             <label for="image" class="form-label">Immagine</label>
             <input name="image" type="file" onchange="showImage(event)"
@@ -68,10 +85,13 @@
               <small class="text-danger">{{ $message }}</small>
             @enderror
           </div>
+
+          {{-- Bottoni Form --}}
           <div class="mb-4">
             <button class="btn btn-primary" type="submit">Aggiungi</button>
             <button class="btn btn-warning" type="reset" onclick="resetImage()">Reset</button>
           </div>
+          <p class="fw-bold text-danger">* Campi Obbligatori</p>
         </form>
       </div>
     </div>
