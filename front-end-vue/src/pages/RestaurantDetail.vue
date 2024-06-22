f1420fbb9cb65314aaa8fb8e430fad945a7c6de3
Oltion-Koceku <koceku17@gmail.com>
2024-06-22 16:32:03 +0200

    [add]product card

--- front-end-vue/src/pages/RestaurantDetail.vue
+++ front-end-vue/src/pages/RestaurantDetail.vue
@@ -1,6 +1,7 @@
 <script>
 import {store} from '../data/store';
 import axios from 'axios';
+import ProductCard from '../components/partials/ProductCard.vue';
 export default {
   name: "restaurantDetail",
   data() {
@@ -15,6 +16,7 @@ export default {
         .then(res => {
           this.restaurant = res.data;
           console.log(res.data);
+          console.log(this.restaurant);
         })
         .catch(err => {
           console.log(err.message);
@@ -23,6 +25,10 @@ export default {
   },
   mounted(){
     this.getApi(store.apiUrl, this.$route.params.slug);
+  },
+
+  components:{
+    ProductCard
   }
 };
 </script>
@@ -32,6 +38,16 @@ export default {
     <!-- <div class="restaurant-image">
       <img v-for="product in restaurant.products" :key="product.id" :src="'http://127.0.0.1:8000/storage/' + product.image" alt="">
     </div> -->
+    <!-- è solo una prova -->
+    <div class="container">
+      <div class=" d-flex row row-cols-3">
+    
+         <ProductCard v-for="product in restaurant.products" :key ="product.id" :product = product />
+    
+     
+    </div>
+    </div>
+    
   </div>
 </template>
 
