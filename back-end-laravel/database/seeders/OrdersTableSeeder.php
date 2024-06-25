c09508ee2bfb76306461dadf7a7ae2adc14f56ec
alessandrodamato <alessandro042004@gmail.com>
2024-06-25 13:04:55 +0200

    [add] migration

--- back-end-laravel/database/seeders/OrdersTableSeeder.php
+++ back-end-laravel/database/seeders/OrdersTableSeeder.php
@@ -18,6 +18,8 @@ class OrdersTableSeeder extends Seeder
     for ($i = 0; $i < 5; $i++) {
 
       $new_order = new Order();
+      $new_order->name = $faker->firstName();
+      $new_order->lastname = $faker->lastName();
       $new_order->amount = floatval(rand(3, 20) . '.' . rand(10, 99));
       $new_order->shipment_address =  $faker->address;
       $new_order->phone =  $faker->randomNumber(9, true);
