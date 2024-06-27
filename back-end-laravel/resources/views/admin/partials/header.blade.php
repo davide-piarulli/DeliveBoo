0c9170b6033b48e9bbccab2302bd382af2229b43
Oltion-Koceku <koceku17@gmail.com>
2024-06-27 12:01:04 +0200

    [rifix]

--- back-end-laravel/resources/views/admin/partials/header.blade.php
+++ back-end-laravel/resources/views/admin/partials/header.blade.php
@@ -5,13 +5,26 @@
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse bg-dark navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <div class="collapse bg-dark navbar-collapse justify-content-between p-4 p-lg-0" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item  text-center">
-                        <a class="nav-link" href="{{ route('admin.dashboard') }}">Home</a>
+                        <a class="nav-link" href="{{ route('admin.dashboard') }}"><i class="fa-solid fa-house"></i> Home</a>
                     </li>
                     <li class="nav-item  text-center mx-5">
-                        <a class="nav-link" target="_blank" href="http://localhost:5174">Vai al sito pubblico</a>
+                        <a class="nav-link" target="_blank" href="http://localhost:5174"><i class="fa-solid fa-satellite-dish"></i> Vai al sito pubblico</a>
+                    </li>
+
+                    <li class="nav-item text-center d-lg-none">
+                      <a href="{{ route('admin.products.index') }}" class="nav-link text-white">
+                        <i class="fa-solid fa-list"></i>
+                        <span class=" d-lg-inline-block ms-1">Elenco Prodotti</span>
+                      </a>
+                    </li>
+                    <li class="nav-item text-center d-lg-none">
+                      <a href="{{ route('admin.orders.index') }}" class="nav-link text-white">
+                        <i class="fa-solid fa-sack-dollar"></i>
+                        <span class=" d-lg-inline-block ms-1">Ordini Ricevuti</span>
+                      </a>
                     </li>
                 </ul>
 
