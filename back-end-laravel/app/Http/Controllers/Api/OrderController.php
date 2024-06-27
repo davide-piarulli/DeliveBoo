23ce242549848479c6c91d230f9b6dcdda8ccf10
alessandrodamato <alessandro042004@gmail.com>
2024-06-27 15:45:20 +0200

    [try to fix] orders

--- back-end-laravel/app/Http/Controllers/Api/OrderController.php
+++ back-end-laravel/app/Http/Controllers/Api/OrderController.php
@@ -9,55 +9,84 @@ use Illuminate\Support\Facades\Mail;
 use Braintree\Gateway;
 use App\Mail\NewOrder;
 use App\Models\Order;
-use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
 
   public function generate(Gateway $gateway)
   {
-      $token = $gateway->clientToken()->generate();
-      $data = [
-          'success' => true,
-          'token' => $token,
-      ];
-      return response()->json($data,200);
+    $token = $gateway->clientToken()->generate();
+    $data = [
+      'success' => true,
+      'token' => $token,
+    ];
+    return response()->json($data, 200);
   }
 
-  public function store_order(Request $request, Gateway $gateway) {
+  public function store(Request $request, Gateway $gateway)
+  {
 
 
     $form_data = $request->all();
 
 
-
     $validator = Validator::make($form_data,
-        // [
-        //   'amount' => 'required|numeric|min:1|max:999',
-        //   'address' => 'required|min:5|max:255',
-        //   'phone' => 'required|numeric|digits:10',
-        //   'notes' => 'max:1000'
-        // ],
-        // [
-        //   'amount.required' => 'Selezionare almeno un prodotto',
-        //   'amount.max' => 'L\'importo non può essere superiore a :max euro.',
+      [
+        'name' => 'required|min:3|max:50',
+        'lastname' => 'required|min:3|max:50',
+        'email' => 'required|email',
+        'amount' => 'min:1|max:999',
+        'address' => 'required|min:5|max:255',
+        'city' => 'required|min:2|max:60',
+        'state' => 'required|size:2',
+        'postal_code' => 'required|size:5',
+        'phone' => 'required|numeric|digits:10',
+        'notes' => 'max:1000'
+      ],
+      [
+        'name.required' => 'Il campo nome è obbligatorio.',
+        'name.min' => 'Il campo nome deve contenere almeno :min caratteri.',
+        'name.max' => 'Il campo nome non può contenere più di :max caratteri.',
 
-        //   'address.required' => 'L\'indirizzo di spedizione è obbligatorio.',
-        //   'address.min' => 'L\'indirizzo di spedizione deve avere almeno :min caratteri.',
-        //   'address.max' => 'L\'indirizzo di spedizione non può superare i :max caratteri.',
+        'lastname.required' => 'Il campo cognome è obbligatorio.',
+        'lastname.min' => 'Il campo cognome deve contenere almeno :min caratteri.',
+        'lastname.max' => 'Il campo cognome non può contenere più di :max caratteri.',
 
-        //   'phone.required' => 'Il numero di telefono è obbligatorio.',
-        //   'phone.digits' => 'Il numero di telefono deve essere composto da :digits cifre.',
+        'email.required' => 'Il campo email è obbligatorio.',
+        'email.email' => 'Inserire una email valida',
 
-        //   'notes.max' => 'Le note non possono superare i :max caratteri.',
-        // ]
-      );
+        'amount.min' => 'L\'importo deve essere almeno di :min euro',
+        'amount.max' => 'L\'importo non può essere maggiore di :max euro',
 
-      if($validator->fails()){
-        $success = false;
-        $errors = $validator->errors();
-        return response()->json(compact('success', 'errors'));
-      }
+        'address.required' => 'L\'indirizzo è obbligatorio.',
+        'address.min' => 'L\'indirizzo deve contenere almeno :min caratteri.',
+        'address.max' => 'L\'indirizzo non può contenere più di :max caratteri.',
+
+        'city.required' => 'Il campo città è obbligatorio.',
+        'city.min' => 'Il campo città deve contenere almeno :min caratteri.',
+        'city.max' => 'Il campo città non può contenere più di :max caratteri.',
+
+        'state.required' => 'Selezionare una provincia.',
+        'state.size' => 'La provincia deve avere :size caratteri',
+
+        'postal_code.required' => 'Il CAP è obbligatorio.',
+        'postal_code.size' => 'Il CAP deve contenere esattamente :size caratteri.',
+
+        'phone.required' => 'Il campo telefono è obbligatorio.',
+        'phone.numeric' => 'Il campo telefono deve essere un numero.',
+        'phone.digits' => 'Il campo telefono deve contenere esattamente :digits cifre.',
+
+        'notes.max' => 'Il campo note non può contenere più di :max caratteri.',
+      ]
+    );
+
+    if ($validator->fails()) {
+
+      $success = false;
+      $errors = $validator->errors();
+      return response()->json(compact('success', 'errors'));
+
+    } else {
 
       $new_order = new Order();
       $new_order->fill($form_data);
@@ -69,47 +98,51 @@ class OrderController extends Controller
 
       $pivotData = [];
       foreach ($form_data['products'] as $product) {
-          $pivotData[$product['id']] = ['quantity' => $product['quantity']];
+        $pivotData[$product['id']] = ['quantity' => $product['quantity']];
       }
       $order->products()->attach($pivotData);
 
-        $result = $gateway->customer()->create([
-            'firstName' => $order->name,
-            'lastName' => $order->lastname,
-            'email' => $order->email,
-            'phone' => $order->phone,
-        ]);
+      $result = $gateway->customer()->create([
+        'firstName' => $order->name,
+        'lastName' => $order->lastname,
+        'email' => $order->email,
+        'phone' => $order->phone,
+      ]);
 
-        $email = $order->email;
+      $email = $order->email;
 
-        $customer_id = $result->customer->id;
+      $customer_id = $result->customer->id;
 
-        $result = $gateway->transaction()->sale([
-            'customerId' => $customer_id,
-            'amount' => $order->amount,
-            'paymentMethodNonce' => $form_data['transaction'],
-            'options' => [
-                'submitForSettlement' => true,
-            ],
-        ]);
+      $result = $gateway->transaction()->sale([
+        'customerId' => $customer_id,
+        'amount' => $order->amount,
+        'paymentMethodNonce' => $form_data['transaction'],
+        'options' => [
+          'submitForSettlement' => true,
+        ],
+      ]);
 
-        if($result->success) {
-          $data = [
-              'success' => true,
-              'message' => 'Transaction was successful'
-          ];
+      if ($result->success) {
+        $data = [
+          'success' => true,
+          'message' => 'Transaction was successful',
+          'result' => $result,
+          'data' => $form_data
+        ];
 
-          Mail::to($email)->send(new NewOrder($order));
+        Mail::to($email)->send(new NewOrder($order));
 
-          return response()->json($data, 200);
-
-      } else{
-          $data = [
-              'success' => false,
-              'message' => 'Transaction failed'
-          ];
-          return response()->json($data, 401);
+        return response()->json($data, 200);
+      } else {
+        $data = [
+          'success' => false,
+          'message' => 'Transaction failed',
+          'result' => $result,
+          'data' => $form_data
+        ];
+        return response()->json($data, 401);
       }
 
+    }
   }
 }
