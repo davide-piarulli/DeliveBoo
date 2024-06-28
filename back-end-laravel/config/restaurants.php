90922bc2dcbec2deb6c6ace149aaa0c25d4c5aae
alessandrodamato <alessandro042004@gmail.com>
2024-06-28 13:10:19 +0200

    [fix] restaurant seeders

--- back-end-laravel/config/restaurants.php
+++ back-end-laravel/config/restaurants.php
@@ -1,145 +1,164 @@
 <?php
 
 return [
-  [
-    'name' => 'Ristorante Bella Italia',
-    'address' => 'Via Roma 123, Roma, Italia',
-    'phone' => '3333334444',
-    'logo' => 'https://example.com/logo_bella_italia.png',
-    'vat_number' => '12345678901'
-  ],
-  [
-    'name' => 'Trattoria da Mario',
-    'address' => 'Piazza Venezia 1, Milano, Italia',
-    'phone' => '3344445555',
-    'logo' => 'https://example.com/logo_da_mario.png',
-    'vat_number' => '10987654321'
-  ],
-  [
-    'name' => 'Osteria del Gusto',
-    'address' => 'Corso Umberto 45, Napoli, Italia',
-    'phone' => '3355556666',
-    'logo' => 'https://example.com/logo_osteria_gusto.png',
-    'vat_number' => '11223344556'
-  ],
-  [
-    'name' => 'Pizzeria La Favorita',
-    'address' => 'Via Dante 89, Firenze, Italia',
-    'phone' => '3366667777',
-    'logo' => 'https://example.com/logo_la_favorita.png',
-    'vat_number' => '22334455667'
-  ],
-  [
-    'name' => 'Café Roma',
-    'address' => 'Largo di Torre Argentina, Roma, Italia',
-    'phone' => '3377778888',
-    'logo' => 'https://example.com/logo_cafe_roma.png',
-    'vat_number' => '33445566778'
-  ],
-  [
-    'name' => 'Ristorante da Giovanni',
-    'address' => 'Via Milano 5, Torino, Italia',
-    'phone' => '3388889999',
-    'logo' => 'https://example.com/logo_da_giovanni.png',
-    'vat_number' => '44556677889'
-  ],
-  [
-    'name' => 'Trattoria La Nonna',
-    'address' => 'Via Garibaldi 22, Bologna, Italia',
-    'phone' => '3399990000',
-    'logo' => 'https://example.com/logo_la_nonna.png',
-    'vat_number' => '55667788990'
-  ],
-  [
-    'name' => 'Osteria del Borgo',
-    'address' => 'Via Manzoni 10, Verona, Italia',
-    'phone' => '3400001111',
-    'logo' => 'https://example.com/logo_del_borgo.png',
-    'vat_number' => '66778899001'
-  ],
-  [
-    'name' => 'Pizzeria Il Vesuvio',
-    'address' => 'Via Toledo 33, Napoli, Italia',
-    'phone' => '3411112222',
-    'logo' => 'https://example.com/logo_il_vesuvio.png',
-    'vat_number' => '77889900112'
-  ],
-  [
-    'name' => 'Café Milano',
-    'address' => 'Piazza Duomo, Milano, Italia',
-    'phone' => '3422223333',
-    'logo' => 'https://example.com/logo_cafe_milano.png',
-    'vat_number' => '88990011223'
-  ],
-  [
-    'name' => 'Ristorante Al Mare',
-    'address' => 'Via Lungomare 15, Genova, Italia',
-    'phone' => '3433334444',
-    'logo' => 'https://example.com/logo_al_mare.png',
-    'vat_number' => '99001122334'
-  ],
-  [
-    'name' => 'Trattoria Il Rustico',
-    'address' => 'Via Firenze 20, Pisa, Italia',
-    'phone' => '3444445555',
-    'logo' => 'https://example.com/logo_il_rustico.png',
-    'vat_number' => '10111223345'
-  ],
-  [
-    'name' => 'Osteria dei Poeti',
-    'address' => 'Via dei Poeti 5, Siena, Italia',
-    'phone' => '3455556666',
-    'logo' => 'https://example.com/logo_dei_poeti.png',
-    'vat_number' => '12131415161'
-  ],
-  [
-    'name' => 'Pizzeria Bella Napoli',
-    'address' => 'Via Napoli 50, Napoli, Italia',
-    'phone' => '3466667777',
-    'logo' => 'https://example.com/logo_bella_napoli.png',
-    'vat_number' => '13141516171'
-  ],
-  [
-    'name' => 'Café Firenze',
-    'address' => 'Piazza della Signoria, Firenze, Italia',
-    'phone' => '3477778888',
-    'logo' => 'https://example.com/logo_cafe_firenze.png',
-    'vat_number' => '14151617181'
-  ],
-  [
-    'name' => 'Ristorante La Scala',
-    'address' => 'Via della Scala 1, Roma, Italia',
-    'phone' => '3488889999',
-    'logo' => 'https://example.com/logo_la_scala.png',
-    'vat_number' => '15161718191'
-  ],
-  [
-    'name' => 'Trattoria del Corso',
-    'address' => 'Corso Italia 2, Trieste, Italia',
-    'phone' => '3499990000',
-    'logo' => 'https://example.com/logo_del_corso.png',
-    'vat_number' => '16171819202'
-  ],
-  [
-    'name' => 'Osteria La Torre',
-    'address' => 'Via della Torre 8, Perugia, Italia',
-    'phone' => '3500001111',
-    'logo' => 'https://example.com/logo_la_torre.png',
-    'vat_number' => '17181920212'
-  ],
-  [
-    'name' => 'Pizzeria La Dolce Vita',
-    'address' => 'Via Veneto 35, Roma, Italia',
-    'phone' => '3511112222',
-    'logo' => 'https://example.com/logo_la_dolce_vita.png',
-    'vat_number' => '18192021222'
-  ],
-  [
-    'name' => 'Café Venezia',
-    'address' => 'Piazza San Marco, Venezia, Italia',
-    'phone' => '3522223333',
-    'logo' => 'https://example.com/logo_cafe_venezia.png',
-    'vat_number' => '19202122232'
-  ]
+    [
+        'name' => 'Ristorante La Piazza',
+        'address' => 'Piazza del Duomo, 20121 Milano MI',
+        'phone' => '3333334444',
+        'logo' => 'https://example.com/logo_la_piazza.png',
+        'vat_number' => '12345678901',
+        'type_ids' => [1, 2]
+    ],
+    [
+        'name' => 'Trattoria da Luigi',
+        'address' => 'Corso Vittorio Emanuele II, 20122 Milano MI',
+        'phone' => '3344445555',
+        'logo' => 'https://example.com/logo_da_luigi.png',
+        'vat_number' => '10987654321',
+        'type_ids' => [1, 4]
+    ],
+    [
+        'name' => 'Sakura Sushi',
+        'address' => 'Via Giapponese 15, Milano, Italia',
+        'phone' => '3355556666',
+        'logo' => 'https://example.com/logo_sakura_sushi.png',
+        'vat_number' => '11223344556',
+        'type_ids' => [3]
+    ],
+    [
+        'name' => 'El Ranchito',
+        'address' => 'Via Monte Napoleone, 20121 Milano MI',
+        'phone' => '3366667777',
+        'logo' => 'https://example.com/logo_el_ranchito.png',
+        'vat_number' => '22334455667',
+        'type_ids' => [4]
+    ],
+    [
+        'name' => 'Taj Mahal Indian Restaurant',
+        'address' => 'Corso Buenos Aires, 20124 Milano MI',
+        'phone' => '3377778888',
+        'logo' => 'https://example.com/logo_taj_mahal.png',
+        'vat_number' => '33445566778',
+        'type_ids' => [5, 9, 16]
+    ],
+    [
+        'name' => 'Olympus Taverna',
+        'address' => 'Piazza della Scala, 20121 Milano MI',
+        'phone' => '3388889999',
+        'logo' => 'https://example.com/logo_olympus_taverna.png',
+        'vat_number' => '44556677889',
+        'type_ids' => [6]
+    ],
+    [
+        'name' => 'Bangkok Street Food',
+        'address' => 'Viale Monza, 20127 Milano MI',
+        'phone' => '3399990000',
+        'logo' => 'https://example.com/logo_bangkok_street_food.png',
+        'vat_number' => '55667788990',
+        'type_ids' => [7, 10]
+    ],
+    [
+        'name' => 'American Diner',
+        'address' => 'Via Torino, 20123 Milano MI',
+        'phone' => '3400001111',
+        'logo' => 'https://example.com/logo_american_diner.png',
+        'vat_number' => '66778899001',
+        'type_ids' => [8, 13]
+    ],
+    [
+        'name' => 'Café España',
+        'address' => 'Corso di Porta Ticinese, 20123 Milano MI',
+        'phone' => '3411112222',
+        'logo' => 'https://example.com/logo_cafe_espana.png',
+        'vat_number' => '77889900112',
+        'type_ids' => [9]
+    ],
+    [
+        'name' => 'Saigon Pho',
+        'address' => 'Corso di Porta Ticinese, 20123 Milano MI',
+        'phone' => '3422223333',
+        'logo' => 'https://example.com/logo_saigon_pho.png',
+        'vat_number' => '88990011223',
+        'type_ids' => [7, 10]
+    ],
+    [
+        'name' => 'Greek Taverna',
+        'address' => 'Via Tortona, 20144 Milano MI',
+        'phone' => '3433334444',
+        'logo' => 'https://example.com/logo_greek_taverna.png',
+        'vat_number' => '99001122334',
+        'type_ids' => [6]
+    ],
+    [
+        'name' => 'Smokehouse BBQ',
+        'address' => 'Via Savona, 20144 Milano MI',
+        'phone' => '3444445555',
+        'logo' => 'https://example.com/logo_smokehouse_bbq.png',
+        'vat_number' => '10111223345',
+        'type_ids' => [13, 14]
+    ],
+    [
+        'name' => 'Mamma Mia Pizzeria',
+        'address' => 'Viale Papiniano, 20123 Milano MI',
+        'phone' => '3455556666',
+        'logo' => 'https://example.com/logo_mamma_mia_pizzeria.png',
+        'vat_number' => '12131415161',
+        'type_ids' => [1]
+    ],
+    [
+        'name' => 'Turkish Delight',
+        'address' => 'Via Fiori Chiari, 20121 Milano MI',
+        'phone' => '3466667777',
+        'logo' => 'https://example.com/logo_turkish_delight.png',
+        'vat_number' => '13141516171',
+        'type_ids' => [11]
+    ],
+    [
+        'name' => 'Lebanese Bistro',
+        'address' => 'Via Orefici, 20123 Milano MI',
+        'phone' => '3477778888',
+        'logo' => 'https://example.com/logo_lebanese_bistro.png',
+        'vat_number' => '14151617181',
+        'type_ids' => [12]
+    ],
+    [
+        'name' => 'Fast & Fresh',
+        'address' => 'Corso Garibaldi, 20121 Milano MI',
+        'phone' => '3488889999',
+        'logo' => 'https://example.com/logo_fast_fresh.png',
+        'vat_number' => '15161718191',
+        'type_ids' => [15]
+    ],
+    [
+        'name' => 'Seafood Sensation',
+        'address' => 'Via Montenapoleone, 20121 Milano MI',
+        'phone' => '3499990000',
+        'logo' => 'https://example.com/logo_seafood_sensation.png',
+        'vat_number' => '16171819202',
+        'type_ids' => [16]
+    ],
+    [
+        'name' => 'Steakhouse Deluxe',
+        'address' => 'Via Dante, 20121 Milano MI',
+        'phone' => '3500001111',
+        'logo' => 'https://example.com/logo_steakhouse_deluxe.png',
+        'vat_number' => '17181920212',
+        'type_ids' => [13]
+    ],
+    [
+        'name' => 'Burger Joint',
+        'address' => 'Corso Como, 20154 Milano MI',
+        'phone' => '3511112222',
+        'logo' => 'https://example.com/logo_burger_joint.png',
+        'vat_number' => '18192021222',
+        'type_ids' => [8, 15]
+    ],
+    [
+        'name' => 'Mediterranean Sunset',
+        'address' => 'Via Solari, 20144 Milano MI',
+        'phone' => '3522223333',
+        'logo' => 'https://example.com/logo_mediterranean_sunset.png',
+        'vat_number' => '19202122232',
+        'type_ids' => [16]
+    ]
 ];
-
--- back-end-laravel/database/seeders/ResturantTypesTableSeeder.php
+++ back-end-laravel/database/seeders/ResturantTypesTableSeeder.php
@@ -14,46 +14,21 @@ class ResturantTypesTableSeeder extends Seeder
    */
   public function run(): void
   {
-    $assignedCombinations = [];
 
-    // Assegna un tipo a ciascun ristorante
+    $config_restaurants = config('restaurants');
+
+    $config_types = [];
+    foreach($config_restaurants as $config_restaurant){
+      $config_types[] = $config_restaurant['type_ids'];
+    }
+
     $restaurants = Restaurant::all();
-    $types = Type::all();
-    foreach ($restaurants as $restaurant) {
-      $type_id = $types->random()->id;
 
-      // Crea una chiave unica per la combinazione
-      $combinationKey = $restaurant->id . '-' . $type_id;
+    foreach($restaurants as $index => $restaurant){
 
-      // Aggiungi la combinazione all'array delle combinazioni assegnate
-      $assignedCombinations[] = $combinationKey;
+        $restaurant->types()->attach($config_types[$index]);
 
-      // Assegna il tipo al ristorante
-      $restaurant->types()->attach($type_id);
     }
 
-    // Conta quante assegnazioni sono già state fatte
-    $assignedCount = count($assignedCombinations);
-
-    // Continua ad assegnare tipi casuali fino a raggiungere 40 assegnazioni totali
-    while ($assignedCount < 55) {
-      do {
-        // Seleziona un ristorante e un tipo a caso
-        $restaurant = Restaurant::inRandomOrder()->first();
-        $type_id = Type::inRandomOrder()->first()->id;
-
-        // Crea una chiave unica per la combinazione
-        $combinationKey = $restaurant->id . '-' . $type_id;
-      } while (in_array($combinationKey, $assignedCombinations));
-
-      // Aggiungi la combinazione all'array delle combinazioni assegnate
-      $assignedCombinations[] = $combinationKey;
-
-      // Assegna il tipo al ristorante
-      $restaurant->types()->attach($type_id);
-
-      // Incrementa il conteggio delle assegnazioni
-      $assignedCount++;
-    }
   }
 }
