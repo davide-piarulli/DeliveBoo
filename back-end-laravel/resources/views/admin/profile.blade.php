d79af4fadfbd43370c8f3df3b11aedac91254911
davide-piarulli <davide.piarulli.1@gmail.com>
2024-06-28 14:05:30 +0100

    [fix] profile section

--- back-end-laravel/resources/views/admin/profile.blade.php
+++ back-end-laravel/resources/views/admin/profile.blade.php
@@ -3,16 +3,22 @@
 
 
 @section('title')
- - Profilo
+  - Profilo
 @endsection
 
 
 
 @section('content')
-
-<div class="container text-center">
-  <h1 class="mb-5">Ciao {{Auth::user()->name}}!</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia laboriosam, voluptas ipsum animi, ut dolores nisi maiores eos asperiores exercitationem natus ducimus id maxime pariatur minima. Quis veritatis quam sapiente quaerat corrupti cumque harum perferendis odio fuga aliquam amet incidunt dolores quod perspiciatis sit maxime voluptates, velit eveniet repudiandae deleniti. Fugiat iure quam repellat, necessitatibus eaque reiciendis commodi suscipit architecto, soluta debitis voluptatem! Provident incidunt a, molestias sint maxime cumque autem natus ullam doloremque, alias saepe consequuntur eum ab deleniti harum omnis commodi animi maiores aperiam nemo reprehenderit, mollitia ad dolor rerum. Alias vel error ratione ipsa, sapiente corrupti provident!</p>
-</div>
-
+  <div class="container my-5">
+    <h1 class="mb-5">Ciao {{ Auth::user()->name }}!</h1>
+    <p>Benvenut* su DeliveBoo! <br>
+      Ecco una semplice guida per iniziare ad utilizzare la piattaforma: <br>
+    <ul>
+      <li>Inizia aggiungendo nuovi prodotti al tuo menù, direttamente dalla sezione "Elenco Prodotti". Inserisci sempre
+        tutti i campi obbligatori, cosicchè il tuo menù sia sempre completo.</li>
+      <li>Quando ricevi un'ordine, lo visualizzerai all'interno della sezione "Ordini Ricevuti". Da lì potrai visualizzare
+        il dettaglio dell'ordine e del cliente.</li>
+    </ul>
+    </p>
+  </div>
 @endsection
