4e7dc3f68fbe16b948564aecf23d6be1747cb5fd
davide-piarulli <davide.piarulli.1@gmail.com>
2024-07-02 13:58:05 +0100

    add icon in header restaurant detail

--- front-end-vue/src/pages/RestaurantDetail.vue
+++ front-end-vue/src/pages/RestaurantDetail.vue
@@ -1,72 +1,98 @@
 <script>
-import { store } from '../data/store';
-import axios from 'axios';
-import ProductCard from '../components/partials/ProductCard.vue';
-import Loader from '../components/partials/Loader.vue';
+import { store } from "../data/store";
+import axios from "axios";
+import ProductCard from "../components/partials/ProductCard.vue";
+import Loader from "../components/partials/Loader.vue";
 export default {
   name: "restaurantDetail",
   components: {
     ProductCard,
-    Loader
+    Loader,
   },
   data() {
     return {
       restaurant: {},
       isLoading: true,
-    }
+    };
   },
   methods: {
     getApi(apiUrl, slug) {
       axios
-        .get(apiUrl + 'restaurant-detail/' + slug)
-        .then(res => {
+        .get(apiUrl + "restaurant-detail/" + slug)
+        .then((res) => {
           this.isLoading = false;
           this.restaurant = res.data;
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err.message);
-        })
+        });
     },
     openModal() {
       this.$refs.openModal.click();
     },
     modalResult(result) {
       store.cartSwitch = result;
-    }
+    },
   },
   mounted() {
     window.scrollTo(0, 0);
     this.getApi(store.apiUrl, this.$route.params.slug);
-    
-  }
+  },
 };
 </script>
 
 <template>
-  <div class="p-5" :class="restaurant.products?.length == 0 ? 'vh-100' : '' ">
-
+  <div class="p-5" :class="restaurant.products?.length == 0 ? 'vh-100' : ''">
     <!-- Modal -->
-    <button ref="openModal" type="button" class="btn btn-primary d-none" data-bs-toggle="modal"
-      data-bs-target="#staticBackdrop"></button>
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-      aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <button
+      ref="openModal"
+      type="button"
+      class="btn btn-primary d-none"
+      data-bs-toggle="modal"
+      data-bs-target="#staticBackdrop"
+    ></button>
+    <div
+      class="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h1 class="modal-title fs-5" id="staticBackdropLabel">DeliveBoo</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
-              @click="modalResult(false)"></button>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              @click="modalResult(false)"
+            ></button>
           </div>
           <div class="modal-body">
-            Non puoi ordinare da più ristoranti contemporaneamente. <br>
-            Vuoi svuotare il carrello e ordinare da questo
-            ristorante?
+            Non puoi ordinare da più ristoranti contemporaneamente. <br />
+            Vuoi svuotare il carrello e ordinare da questo ristorante?
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
-              @click="modalResult(false)">Annulla</button>
-            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="modalResult(true)">Cambia
-              Ristorante</button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+              @click="modalResult(false)"
+            >
+              Annulla
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-bs-dismiss="modal"
+              @click="modalResult(true)"
+            >
+              Cambia Ristorante
+            </button>
           </div>
         </div>
       </div>
@@ -74,65 +100,88 @@ export default {
     <!-- /Modal -->
 
     <Loader v-if="isLoading" :height1="107" :height2="0" />
-    <div class="container h-100" v-else-if="restaurant.products?.length == 0 ">
-
+    <div class="container h-100" v-else-if="restaurant.products?.length == 0">
       <div class="w-100 h-100 d-flex justify-content-center align-items-center">
-        <h1 class="text-center">Il ristorante <span class="text-danger">{{ restaurant.name }}</span> non ha ancora prodotti</h1>
+        <h1 class="text-center">
+          Il ristorante
+          <span class="text-danger">{{ restaurant.name }}</span> non ha ancora
+          prodotti
+        </h1>
       </div>
-
     </div>
     <div v-else class="container">
-      <div class="restaurant-data d-flex justify-content-center justify-content-lg-start align-items-center flex-wrap rounded-5 p-3 p-xl-5">
+      <div
+        class="restaurant-data d-flex justify-content-center justify-content-lg-start align-items-center flex-wrap rounded-5 p-3 p-xl-5"
+      >
         <div class="restaurant-logo mb-3 mb-md-0 rounded-5 overflow-hidden">
           <img
-            :src="restaurant.logo ? 'http://127.0.0.1:8000/storage/' + restaurant.logo : '/default-logo.png'"
+            :src="
+              restaurant.logo
+                ? 'http://127.0.0.1:8000/storage/' + restaurant.logo
+                : '/default-logo.png'
+            "
             onerror="this.src = '/default-logo.png'"
             :alt="restaurant.name + ' logo'"
             class="img-fluid"
-            :class="{'six-fingers' : restaurant.name == 'Trattoria da Luigi'}"
+            :class="{ 'six-fingers': restaurant.name == 'Trattoria da Luigi' }"
           />
         </div>
         <div class="ps-3 ps-lg-5">
           <h1>{{ restaurant.name }}</h1>
-          <h4>{{ restaurant.address }}</h4>
-          <h4>Tel: <a class="text-black text-decoration-none" :href="`tel:+39${restaurant.phone}`">{{ restaurant.phone }}</a></h4>
+          <h4>
+            <i class="fa-solid fa-location-dot"></i> {{ restaurant.address }}
+          </h4>
+          <h4>
+            <a
+              class="text-black text-decoration-none"
+              :href="`tel:+39${restaurant.phone}`"
+            >
+              <i class="fa-solid fa-phone"></i> {{ restaurant.phone }}</a
+            >
+          </h4>
         </div>
       </div>
-      <div class=" d-flex row">
-        <ProductCard @openModal="openModal()" v-for="product in restaurant.products" :key="product.id"
-          :product=product />
+      <div class="d-flex row">
+        <ProductCard
+          @openModal="openModal()"
+          v-for="product in restaurant.products"
+          :key="product.id"
+          :product="product"
+        />
       </div>
     </div>
-
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 .modal-content {
-  background-color: #2B3D4F;
+  background-color: #2b3d4f;
   color: white;
-  .modal-header, .modal-footer{
+  .modal-header,
+  .modal-footer {
     border: 0;
   }
 
-  .btn.btn-primary, .btn-close {
+  .btn.btn-primary,
+  .btn-close {
     border: 0;
-    background-color: #A7C0E2 !important;
+    background-color: #a7c0e2 !important;
     color: black;
   }
 
   .btn.btn-secondary {
     border: 0;
-    background-color: #60758E !important;
+    background-color: #60758e !important;
   }
 }
 
-.restaurant-data{
-  background-color: #B9D3F7;
-  .restaurant-logo{
+.restaurant-data {
+  background-color: #b9d3f7;
+  .restaurant-logo {
     width: 200px;
   }
+  i {
+    color: #2b3d4f;
+  }
 }
-
 </style>
